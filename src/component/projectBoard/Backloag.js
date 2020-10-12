bc9b0f97b27d6977f99d068fa4b555b561b52dd9
Arunprem <kuttunick@gmail.com>
2020-10-12 17:59:38 +0530

    project board competed

---
+++ src/component/projectBoard/Backloag.js
@@ -0,0 +1,49 @@
+import React, { Component } from "react";
+import ProjectTask from "./projectTasks/ProjectTask";
+
+class Backloag extends Component {
+  render() {
+    const { project_task_props } = this.props;
+    const tasks = project_task_props.map((project_task) => (
+      <ProjectTask key={project_task.id} project_task={project_task} />
+    ));
+
+    let todoItems = [];
+    let inProgressItems = [];
+    let doneItems = [];
+
+    for (let i = 0; i < tasks.length; i++) {
+      console.log(tasks[i]);
+    }
+    return (
+      <div className="container">
+        <div className="row">
+          <div className="col-md-4">
+            <div className="card text-center mb-2">
+              <div className="card-header bg-secondary text-white">
+                <h3>TO DO</h3>
+              </div>
+            </div>
+
+            {tasks}
+          </div>
+          <div className="col-md-4">
+            <div className="card text-center mb-2">
+              <div className="card-header bg-primary text-white">
+                <h3>In Progress</h3>
+              </div>
+            </div>
+          </div>
+          <div className="col-md-4">
+            <div className="card text-center mb-2">
+              <div className="card-header bg-success text-white">
+                <h3>Done</h3>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+export default Backloag;
--- src/component/projectBoard/ProjectBoard.js
+++ src/component/projectBoard/ProjectBoard.js
@@ -1,72 +1,39 @@
-import React, { Component } from 'react';
-import {Link} from "react-router-dom";
+import React, { Component } from "react";
+import { Link } from "react-router-dom";
+import Backloag from "./Backloag";
+import { connect } from "react-redux";
+import PropTypes from "prop-types";
+import { getBackLog } from "../../actions/backlogActions";
+import ProjectTask from "./projectTasks/ProjectTask";
 
 class ProjectBoard extends Component {
-    render() {
+  componentDidMount() {
+    const { id } = this.props.match.params;
+    this.props.getBackLog(id);
+  }
+  render() {
+    const { id } = this.props.match.params;
 
-        const {id}=this.props.match.params;
-        return (
-            <div className="container">
+    const { project_tasks } = this.props.backlog;
+    return (
+      <div className="container">
         <Link to={`/addProjectTask/${id}`} className="btn btn-primary mb-3">
-            <i className="fa fa-plus-circle"> Create Project Task</i>
+          <i className="fa fa-plus-circle"> Create Project Task</i>
         </Link>
         <br />
         <hr />
-    
-        <div className="container">
-            <div className="row">
-                <div className="col-md-4">
-                    <div className="card text-center mb-2">
-                        <div className="card-header bg-secondary text-white">
-                            <h3>TO DO</h3>
-                        </div>
-                    </div>
-
-                   
-                    <div className="card mb-1 bg-light">
-
-                        <div className="card-header text-primary">
-                            ID: projectSequence -- Priority: priorityString
-                        </div>
-                        <div className="card-body bg-light">
-                            <h5 className="card-title">project_task.summary</h5>
-                            <p className="card-text text-truncate ">
-                                project_task.acceptanceCriteria
-                            </p>
-                            <a href="update" className="btn btn-primary">
-                                View / Update
-                            </a>
-
-                            <button className="btn btn-danger ml-4">
-                                Delete
-                            </button>
-                        </div>
-                    </div>
-
-               
-                </div>
-                <div className="col-md-4">
-                    <div className="card text-center mb-2">
-                        <div className="card-header bg-primary text-white">
-                            <h3>In Progress</h3>
-                        </div>
-                    </div>
-               
-                </div>
-                <div className="col-md-4">
-                    <div className="card text-center mb-2">
-                        <div className="card-header bg-success text-white">
-                            <h3>Done</h3>
-                        </div>
-                    </div>
-                  
-                </div>
-            </div>
-        </div>
-
-      
-    </div>
-        )
-    }
+        <Backloag project_task_props={project_tasks} />
+      </div>
+    );
+  }
 }
-export default  ProjectBoard;
+
+ProjectBoard.propTypes = {
+  backlog: PropTypes.object.isRequired,
+  getBackLog: PropTypes.func.isRequired,
+};
+
+const mapStatetoPtops = (state) => ({
+  backlog: state.backlog,
+});
+export default connect(mapStatetoPtops, { getBackLog })(ProjectBoard);
